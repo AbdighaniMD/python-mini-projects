85188ff42c5eb6817068b060f9603683727797bd
AbdighaniMD <abdighanimd@outlook.com>
2023-10-23 20:20:21 +0100

    Email Sender

---
+++ 05-project/emailSender.py
@@ -0,0 +1,14 @@
+import smtplib
+
+recipentEmail = str(input("Enter the email of recipent: "))
+recipentContent = str(input("Enter the email of content: "))
+
+def sendEmail(recipentEmail, recipentContent):
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehio()
+    server.starttls()
+    server.login('abdighanimd02@gmail.com', 'password')
+    server.sendmail('abdighanimd02@gmail.com', recipentEmail, recipentContent)
+    server.close()
+
+sendEmail(recipentEmail, recipentContent)
